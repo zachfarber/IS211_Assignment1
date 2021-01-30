6710683ba7714b0f97fc4305dd418cd8e5d4845e
zachfarber <73371719+zachfarber@users.noreply.github.com>
2021-01-30 17:05:54 -0500

    updated part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,10 +1,17 @@
 
 class Book:
-    def __init(self, author, title):
+    def __init__(self, author='', title=''):
+        self.author = author
+        self.title = title
         pass
-    
+
     def display(self):
+        print(self.title + ' written by ' + self.author)
         pass
-        
+
+
+Book('John Steinbeck', 'Of Mice and Men').display()
+Book('Harper Lee', 'To Kill a Mockingbird').display()
+
 if __name__ == "__main__":
     pass
